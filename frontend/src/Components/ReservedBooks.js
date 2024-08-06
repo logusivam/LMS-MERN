7c4ab461dae663b5b3a93a8a993c13d1cd40aacf
Loganathan G P <logusivam26@gmail.com>
2024-08-06 16:00:39 +0000

    Aug 6, 2024, 9:30 PM

--- frontend/src/Components/ReservedBooks.js
+++ frontend/src/Components/ReservedBooks.js
@@ -14,32 +14,32 @@ function ReservedBooks() {
                 <tr>
                     <td>Pranav</td>
                     <td>Rich Dad Poor Dad</td>
-                    <td>12/7/2021</td>
+                    <td>12/7/2024</td>
                 </tr>
                 <tr>
                     <td>Sashank</td>
                     <td>The Subtle Art</td>
-                    <td>10/7/2021</td>
+                    <td>10/7/2024</td>
                 </tr>
                 <tr>
                     <td>Tanishq</td>
                     <td>Wings Of Fire</td>
-                    <td>15/9/2021</td>
+                    <td>15/9/2024</td>
                 </tr>
                 <tr>
                     <td>Akhil</td>
                     <td>The Secret</td>
-                    <td>02/9/2021</td>
+                    <td>02/9/2024</td>
                 </tr>
                 <tr>
                     <td>Surya</td>
                     <td>Bad Guys</td>
-                    <td>21/7/2021</td>
+                    <td>21/7/2024</td>
                 </tr>
                 <tr>
                     <td>Dinesh</td>
                     <td>Giovanni Rovelli</td>
-                    <td>02/7/2021</td>
+                    <td>02/7/2024</td>
                 </tr>
             </table>
         </div>
