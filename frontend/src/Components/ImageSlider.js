9c0eea88f3312e21766f467d55514ba38f072492
Loganathan G P <logusivam26@gmail.com>
2024-08-06 16:08:39 +0000

    Aug 6, 2024, 9:38 PM

--- frontend/src/Components/ImageSlider.js
+++ frontend/src/Components/ImageSlider.js
@@ -13,7 +13,7 @@ function ImageSlider() {
                         alt="First slide"
                     />
                     <Carousel.Caption>
-                        <h3>First slide label</h3>
+                        <h3>PLACE 1</h3>
                         <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
@@ -24,7 +24,7 @@ function ImageSlider() {
                         alt="Second slide"
                     />
                     <Carousel.Caption>
-                        <h3>Second slide label</h3>
+                        <h3>PLACE 2</h3>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
@@ -35,7 +35,7 @@ function ImageSlider() {
                         alt="Third slide"
                     />
                     <Carousel.Caption>
-                        <h3>Third slide label</h3>
+                        <h3>PLACE 3</h3>
                         <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
