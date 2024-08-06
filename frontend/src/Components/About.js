efd625e59adebfdf4ff5ef6d2633736515d37298
Loganathan G P <logusivam26@gmail.com>
2024-08-06 15:11:08 +0000

    Aug 6, 2024, 8:41 PM

--- frontend/src/Components/About.js
+++ frontend/src/Components/About.js
@@ -13,7 +13,7 @@ function About() {
                     <p className="about-text">
                         Lorem Ipsum is simply dummy text of the printing and typesetting 
                         industry. Lorem Ipsum has been the industry's standard dummy text 
-                        ever since the 1500s, when an unknown printer took a galley of type 
+                        ever sine the 1500s, when an unknown printer took a galley of type 
                         and scrambled it to make a type specimen book. It has survived not 
                         only five centuries.<br/>
                         <br/>
