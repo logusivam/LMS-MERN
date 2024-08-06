6124fcf13cacdcef679e78c846a01d380478a3b8
Loganathan G P <logusivam26@gmail.com>
2024-08-06 16:02:02 +0000

    Aug 6, 2024, 9:32 PM

--- frontend/src/Components/Stats.js
+++ frontend/src/Components/Stats.js
@@ -10,17 +10,17 @@ function Stats() {
             <div className='stats-block'>
                 <LibraryBooksIcon className='stats-icon' style={{ fontSize:80 }}/>
                 <p className='stats-title'>Total Books</p>
-                <p className='stats-count'>3254</p>
+                <p className='stats-count'>10,000</p>
             </div>
             <div className='stats-block'>
                 <LocalLibraryIcon className='stats-icon' style={{ fontSize:80 }}/>
                 <p className='stats-title'>Total Members</p>
-                <p className='stats-count'>254</p>
+                <p className='stats-count'>400</p>
             </div>
             <div className='stats-block'>
                 <BookIcon className='stats-icon' style={{ fontSize:80 }}/>
                 <p className='stats-title'>Reservations</p>
-                <p className='stats-count'>54</p>
+                <p className='stats-count'>540</p>
             </div>
         </div>
     )
