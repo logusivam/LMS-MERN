3858fb1b23c1148752cd179c4013dbe4511d20b1
Loganathan G P <logusivam26@gmail.com>
2024-08-06 15:42:20 +0000

    Aug 6, 2024, 9:12 PM

--- frontend/src/Components/Footer.js
+++ frontend/src/Components/Footer.js
@@ -36,7 +36,7 @@ function Footer() {
                 </div>
                 <div className="contact-social" >
                     <a href='#home' className='social-icon'><TwitterIcon style={{ fontSize: 40,color:"rgb(283,83,75)"}} /></a>
-                    <a href='#home' className='social-icon'><LinkedInIcon style={{ fontSize: 40,color:"rgb(283,83,75)"}} /></a>
+                    <a href='https://www.linkedin.com/in/loganathan26/' className='social-icon'><LinkedInIcon style={{ fontSize: 40,color:"rgb(283,83,75)"}} /></a>
                     <a href='#home' className='social-icon'><TelegramIcon style={{ fontSize: 40,color:"rgb(283,83,75)"}} /></a>
                     <a href='#home' className='social-icon'><InstagramIcon style={{ fontSize: 40,color:"rgb(283,83,75)"}} /></a>
                 </div>
